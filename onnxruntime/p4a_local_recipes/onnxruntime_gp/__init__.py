8e38e7c6bf99c0d0d7a7d3d610048b17d53a0c59
Somnath Das <somnath.das@daslearning.in>
2025-09-15 03:18:46 +0000

    will use make to check

--- onnxruntime/p4a_local_recipes/onnxruntime_gp/__init__.py
+++ onnxruntime/p4a_local_recipes/onnxruntime_gp/__init__.py
@@ -16,12 +16,9 @@ class OnnxRuntimeRecipe(CythonRecipe):
 
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super().get_recipe_env(arch, with_flags_in_cc)
-        env["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
         env['CPPFLAGS'] += ' -Wno-unused-variable'
 
         # Make sure cross python/protoc are visible
-        env["PYTHON_EXECUTABLE"] = sh.which("python3")
-        env["ONNX_CUSTOM_PROTOC_EXECUTABLE"] = sh.which("protoc")
         print(f"Host Python: {self.ctx.hostpython}")
         print(f"NDK Dir: {self.ctx.ndk_dir}")
         return env
@@ -37,17 +34,19 @@ class OnnxRuntimeRecipe(CythonRecipe):
         dist_dir = join(build_dir, "dist")
         python_site_packages = self.ctx.get_site_packages_dir(arch)
         python_include_numpy = join(python_site_packages, 'numpy', 'core', 'include')
+        toolchain_file = join(self.ctx.ndk_dir,
+                                'build/cmake/android.toolchain.cmake')
         protoc_path = sh.which("protoc")
-        python_path = sh.which("python")
+        python_path = sh.which("python3")
         shprint(sh.mkdir, "-p", capi_dir)
 
         cmake_args = [
             "cmake",
             cmake_dir,
             f"-DCMAKE_INSTALL_PREFIX={capi_dir}",
-            f"-DCMAKE_TOOLCHAIN_FILE={self.ctx.ndk_dir}/build/cmake/android.toolchain.cmake",
+            f"-DCMAKE_TOOLCHAIN_FILE={toolchain_file}",
             f"-DANDROID_ABI={arch.arch}",
-            f"-DANDROID_NATIVE_API_LEVEL={self.ctx.ndk_api}",
+            "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
             "-Donnxruntime_ENABLE_PYTHON=ON",
             "-Donnxruntime_BUILD_SHARED_LIB=OFF",
             "-DPYBIND11_USE_CROSSCOMPILING=TRUE",
@@ -62,22 +61,20 @@ class OnnxRuntimeRecipe(CythonRecipe):
 
         with current_directory(build_dir):
             shprint(sh.Command("cmake"), *cmake_args, _env=env)
-            shprint(sh.Command("cmake"), "--build", ".", "--target", "generate_build_and_package_info", _env=env)
-            shprint(sh.Command("cmake"), "--build", ".", _env=env)
-            shprint(sh.Command("cmake"), "--install", ".", _env=env)
-            shprint("tree", build_dir)
+            shprint(sh.make, '-j' + str(cpu_count()), _env=env)
+            shprint(sh.make, 'install', _env=env)
+            #shprint(sh.Command("cmake"), "--build", ".", _env=env)
+            #shprint(sh.Command("cmake"), "--install", ".", _env=env)
 
-            print("Listing the cmake directory:")
-            shprint(sh.ls, "-R", cmake_dir)
-            print("Listing the build directory:")
-            shprint(sh.ls, "-R", build_dir)
+            print("=== Installed capi contents ===")
+            shprint(sh.ls, "-R", capi_dir)
             # Build wheel
             hostpython = sh.Command(self.ctx.hostpython)
             print(f"Host Python: {self.ctx.hostpython}")
             hostpython = sh.Command(self.hostpython_location)
             print(f"Host Python: {self.hostpython_location}")
             shprint(python_path, "-m", "build", "--wheel", "--no-isolation", _env=env)
-
+            shprint("tree", build_dir)
             # Install wheel into target python site-packages
             pyver = self.ctx.python_recipe.version[0:4].replace(".", "")
             whl_pattern = f"onnxruntime-{self.version}-cp{pyver}-*.whl"
